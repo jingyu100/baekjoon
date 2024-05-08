5ea17fd10359eabc8a3bae6b58ec537e791564d3
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-05-08 23:39:26 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int up = in.nextInt();		
+        int down = in.nextInt();	
+        int height = in.nextInt(); 	
+        int day = (height - down) / (up - down);
+        if ((height - down) % (up - down) != 0) {
+            day++;
+        }
+        System.out.println(day);
+    }
+}
