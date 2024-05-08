08552cfc657d03ac4bdf6c4b594e2a7160851f17
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-05-08 21:24:58 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        int len = word.length();
+
+        for(int i=0; i<len-1; i++) {
+            if(word.charAt(i) != word.charAt(len-i-1)) {
+                System.out.println("0");
+                return;
+            }
+        }
+        System.out.println("1");
+    }
+}
