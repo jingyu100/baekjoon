bcd1bcce9acd0f643f6cad0e2a9e8e85bfeee43f
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-05-08 21:12:08 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 568 ms, Memory: 23360 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = sc.nextInt();
+        for(int i = 1; i <= count ; i++) {
+            for(int j = 0; j < count-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+
+        for(int i = count-1; i >= 0 ; i--) {
+            for(int j = 0; j < count-i; j++)
+                System.out.print(" ");
+            for(int j = 0; j < i*2-1; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
