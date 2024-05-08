2dc1ff9d8d3c1802973fa7af4def1f56270f73a8
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-05-08 19:58:02 +0900

    [Bronze II] Title: 다이얼, Time: 196 ms, Memory: 17612 KB -BaekjoonHub

---
+++ Java/백준/Bronze/5622. 다이얼/다이얼.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.next();
+        int sec = 0;
+        for (int i = 0; i < word.length(); i++) {
+            switch (word.charAt(i)) {
+                case 'A' :
+                case 'B' :
+                case 'C' :
+                    sec += 3;
+                    break;
+                case 'D' :
+                case 'E' :
+                case 'F' :
+                    sec += 4;
+                    break;
+                case 'G' :
+                case 'H' :
+                case 'I' :
+                    sec += 5;
+                    break;
+                case 'J' :
+                case 'K' :
+                case 'L' :
+                    sec += 6;
+                    break;
+                case 'M' :
+                case 'N' :
+                case 'O' :
+                    sec += 7;
+                    break;
+                case 'P' :
+                case 'Q' :
+                case 'R' :
+                case 'S' :
+                    sec += 8;
+                    break;
+                case 'T' :
+                case 'U' :
+                case 'V' :
+                    sec += 9;
+                    break;
+                case 'W' :
+                case 'X' :
+                case 'Y' :
+                case 'Z' :
+                    sec += 10;
+                    break;
+            }
+        }
+
+        System.out.println(sec);
+    }
+}
