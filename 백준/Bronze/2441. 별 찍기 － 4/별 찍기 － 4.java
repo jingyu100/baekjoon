8b486fb8a14a4d735fa39d209ee6d7058d90e661
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-06-16 10:50:47 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 364 ms, Memory: 18828 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x; j++) {
+                if(j >= i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
