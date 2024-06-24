e825abe2cbb646fb06ff8e03d3d1622c8b1bd7d2
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-06-25 08:29:49 +0900

    [Bronze I] Title: 더하기 사이클, Time: 208 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int cnt = 0;
+		int copy = n;
+		while (true) {
+			n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+			cnt++;
+			if (copy == n) {
+				break;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
