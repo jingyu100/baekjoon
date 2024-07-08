55e75c5ab2a7a8dd70339a3eaf0b13b4f56e0789
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-07-08 09:45:40 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 220 ms, Memory: 18360 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.print(":fan:");
+        System.out.print(":fan:");
+        System.out.print(":fan:");
+        System.out.println("");
+        System.out.print(":fan:");
+        System.out.print(":" + id + ":");
+        System.out.print(":fan:");
+        System.out.println("");
+        System.out.print(":fan:");
+        System.out.print(":fan:");
+        System.out.print(":fan:");
+    }
+}
