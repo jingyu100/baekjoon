61cc29376af7debdd828f155b0d3a6b9472bf88d
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-07-28 15:43:11 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 220 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] result = new int[5];
+        int p = scanner.nextInt();
+        int area = scanner.nextInt();
+        int cnt = p * area;
+        for (int i = 0; i < 5; i++) {
+            int temp = scanner.nextInt();
+            result[i] = temp - cnt;
+        }
+        for (int i : result) {
+            System.out.println(i);
+        }
+    }
+}
