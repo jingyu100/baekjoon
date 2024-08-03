feeb9e36b52c16a0d0edea2be739a134fde128ec
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-08-04 01:40:36 +0900

    [Bronze II] Title: 커트라인, Time: 244 ms, Memory: 20664 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] list = new int[n];
+        for (int i = 0; i < n; i++) {
+            list[i] = sc.nextInt();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (list[i] < list[j]) {
+                    int temp = list[i];
+                    list[i] = list[j];
+                    list[j] = temp;
+                }
+            }
+        }
+        System.out.print(list[k-1]);
+    }
+}
