aea95456ba2a45bc96a14e8e38c3b04a864a55a6
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-08-04 06:37:07 +0900

    [Silver V] Title: 카드1, Time: 220 ms, Memory: 18068 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            queue.add(i);
+        }
+        while (queue.size() > 1) {
+            System.out.print(queue.poll() + " ");
+            queue.add(queue.poll());
+        }
+        System.out.print(queue.poll());
+    }
+}
