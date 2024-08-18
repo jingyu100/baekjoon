03026525e440aec385dbe3e29a0690f4107b7cce
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-08-18 10:34:51 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 256 ms, Memory: 1132 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,42 +1,34 @@
-#include <algorithm>
-#include <memory.h>
-#include <stack>
+//#include <algorithm>
+//#include <memory.h>
+//#include <stack>
 #include <stdio.h>
-#include <string.h>
-using namespace std;
+//#include <string.h>
+//using namespace std;
 
 int q[5001];
-int ans[5001];
 
 int main() {
     int n, k, cnt = 0, anscnt = 0;
     scanf("%d%d", &n, &k);
-    for (int i = 1; i <= n; ++i) {
-        q[i] = i;
-    }
+    for (int i = 1; i <= n; ++i) q[i] = i;
     int num = k;
     printf("<");
     while (1) {
-        ans[anscnt++] = q[num];
-        if (anscnt == n) break;
+        anscnt++;
+        if(anscnt == n) {
+            printf("%d>", q[num]);
+            break;
+        }
+        printf("%d, ",q[num]);
         q[num] = 0;
         while (1) {
-            if (cnt == k) {
-                break;
-            }
+            if (cnt == k) break;
             num++;
             if (num > n) num = 1;
-            if (q[num] > 0) {
-                cnt++;
-            }
+            if (q[num] > 0) cnt++;
         }
         if (cnt == 0) break;
-        else
-            cnt = 0;
+        cnt = 0;
     }
-    for (int i = 0; i < n - 1; ++i) {
-        printf("%d, ", ans[i]);
-    }
-    printf("%d>", ans[n - 1]);
     return 0;
 }
