6def04ea4971a5b4c01e7f41c73376bd7a80b028
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-08-31 11:57:14 +0900

    [Gold IV] Title: 그룹 나누기 (Subset), Time: 0 ms, Memory: 1188 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/16132. 그룹 나누기 （Subset）/그룹 나누기 （Subset）.cc
+++ 백준/Gold/16132. 그룹 나누기 （Subset）/그룹 나누기 （Subset）.cc
@@ -3,21 +3,21 @@
 
 using namespace std;
 
-int n, list[1300], sum;
+long long int sum, list[10000], n;
 
 int main() {
-    scanf("%d", &n);
-    if((n * (n + 1) / 2) % 2 == 1) {
+    scanf("%lld", &n);
+    if ((n * (n + 1) / 2) % 2 == 1) {
         printf("0");
         exit(0);
     }
     sum = (n * (n + 1) / 2) / 2;
     list[0] = 1;
-    for (int i = 1; i <= n; ++i) {
-        for (int j = sum; j >= i; --j) {
+    for (long long int i = 1; i <= n; ++i) {
+        for (long long int j = sum; j >= i; --j) {
             list[j] += list[j - i];
         }
     }
-    printf("%d\n", list[sum] / 2);
+    printf("%lld", list[sum] / 2);
     return 0;
 }
