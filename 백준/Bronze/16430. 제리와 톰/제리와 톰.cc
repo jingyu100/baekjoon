e2d33faab83de919c53a08586ea49defd3c1ea81
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-09-16 21:12:19 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <stdio.h>
+#include <vector>
+#include <cstring>
+#include <queue>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+int a, b;
+
+int main() {
+    scanf("%d%d", &a, &b);
+    printf("%d %d", b - a, b);
+    return 0;
+}
