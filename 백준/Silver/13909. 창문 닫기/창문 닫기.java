4cb7d5d35200b6c78a261fc1ac3fc8a0ffe1b01d
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-10-22 22:22:19 +0900

    [Silver V] Title: 창문 닫기, Time: 168 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+import static java.lang.Math.sqrt;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println((int) sqrt(n));
+    }
+}
