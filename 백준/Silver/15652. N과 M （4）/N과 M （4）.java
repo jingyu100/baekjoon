fb5df04f21cc0f88ef478217bfae13378d1fe9ec
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-10-31 11:31:41 +0900

    [Silver III] Title: N과 M (4), Time: 132 ms, Memory: 16708 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int[] seq;
+    private static int n, m;
+    private static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        seq = new int[m];
+        dfs(1, 0);
+        System.out.print(sb);
+    }
+
+    private static void dfs(int start, int depth) {
+        if (depth == m) {
+            for (int i = 0; i < seq.length; i++) {
+                sb.append(seq[i]).append(' ');
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = start; i <= n; i++) {
+            seq[depth] = i;
+            dfs(i, depth + 1);
+        }
+    }
+}
