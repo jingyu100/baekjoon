6eaa2cd0bbb4233574bb038ac156a7882b650d4b
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2024-12-14 22:49:15 +0900

    [Bronze III] Title: 부호, Time: 672 ms, Memory: 116104 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        for (int t = 0; t < 3; t++) {
+            int n = Integer.parseInt(br.readLine());
+            BigInteger sum = BigInteger.ZERO;
+            
+            for (int i = 0; i < n; i++) {
+                BigInteger num = new BigInteger(br.readLine());
+                sum = sum.add(num);
+            }
+            
+            if (sum.compareTo(BigInteger.ZERO) > 0) System.out.println("+");
+            else if (sum.compareTo(BigInteger.ZERO) < 0) System.out.println("-"); 
+            else 
+                System.out.println("0");
+        
+        }
+    }
+}
