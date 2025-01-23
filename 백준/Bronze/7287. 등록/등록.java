2351a3d9f8cec73cd41e0a530592048e636b061e
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2025-01-23 19:03:36 +0900

    [Bronze V] Title: 등록, Time: 96 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("409");
+        System.out.println("paka0100");
+    }
+}
