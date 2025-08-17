8fea68944e7a77f1fdd367472cd10d3ad072a86c
jingyu100 <168957675+jingyu100@users.noreply.github.com>
2025-08-17 22:26:13 +0900

    [Bronze II] Title: 시그마, Time: 180 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/2355. 시그마/시그마.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        if (a > b) System.out.println((a - b + 1) * (b + a) / 2);
+        else System.out.println((b - a + 1) * (b + a) / 2);
+    }
+}
